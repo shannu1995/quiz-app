cc96db65c4b2890f268cdba8f0f9d4bcdf497d20
shannu1995 <sivapurapu95@gmail.com>
2025-03-12 08:07:51 +0530

    Handing error code 500.

--- app/errors.py
+++ app/errors.py
@@ -5,3 +5,8 @@ from flask import render_template
 def not_found_error(error):
     print()
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    print()
+    return render_template('500.html'), 500
