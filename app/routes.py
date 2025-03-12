c53af80f3bb47db8f17960bac42bef04be4c24ba
shannu1995 <sivapurapu95@gmail.com>
2025-03-12 07:07:44 +0530

    User can now download and view the Wikipedia Data.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,25 @@
 from app import app
 from flask import render_template, url_for
+import pandas as pd
+import requests
+from io import StringIO
 
 @app.route('/')
 @app.route('/index')
 def index():
     title = "Georgraphy Quiz App"
     return render_template('index.html', title=title)
+
+@app.route('/refresh-data', methods=['GET'])
+def refresh_table_data():
+    popular_pages_url = "https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Countries/Popular_pages"
+    request_1 = requests.get(popular_pages_url)
+    popularity_data = StringIO(request_1.text)
+    popularity_df = pd.read_html(popularity_data)[0]
+    national_capitals_url = "https://en.wikipedia.org/wiki/List_of_national_capitals"
+    request_2 = requests.get(national_capitals_url)
+    capitals_data = StringIO(request_2.text)
+    capitals_df = pd.read_html(capitals_data)[1]
+    capitals_quiz_data = pd.merge(popularity_df, capitals_df, how='inner', left_on='Page title', right_on='Country/Territory')
+    capitals_quiz_data_relevant_only = capitals_quiz_data[['Country/Territory', 'City/Town', "Continent", "Views"]].copy()
+    return render_template('refresh-data.html', data=capitals_quiz_data_relevant_only.to_html())
