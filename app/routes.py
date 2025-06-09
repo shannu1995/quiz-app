3ab01c487380cf80000b54ce17ef7be398691575
shannu1995 <sivapurapu95@gmail.com>
2025-06-09 17:26:21 +1000

    Removed refresh feature for elastic beanstalk.

--- app/routes.py
+++ app/routes.py
@@ -39,20 +39,23 @@ def view_existing_data():
     
 @app.route('/refresh-data', methods=['GET'])
 def refresh_table_data():
-    popular_pages_url = "https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Countries/Popular_pages"
-    request_1 = requests.get(popular_pages_url)
-    popularity_data = StringIO(request_1.text)
-    popularity_df = pd.read_html(popularity_data)[0]
-    national_capitals_url = "https://en.wikipedia.org/wiki/List_of_national_capitals"
-    request_2 = requests.get(national_capitals_url)
-    capitals_data = StringIO(request_2.text)
-    capitals_df = pd.read_html(capitals_data)[1]
-    capitals_quiz_data = pd.merge(popularity_df, capitals_df, how='inner', left_on='Page title', right_on='Country/Territory')
-    capitals_quiz_data_relevant_only = capitals_quiz_data[['Country/Territory', 'City/Town', "Continent", "Views"]].copy()
-    capitals_quiz_data_relevant_only["Last Updated"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    with get_connection() as conn:
-        capitals_quiz_data_relevant_only.to_sql(Config.CAPITALS_TABLE_NAME, conn, if_exists='replace', index=False)
-    return render_template('refresh-data.html', data=capitals_quiz_data_relevant_only.to_html())
+    if DB_TYPE == "sqlite":
+        popular_pages_url = "https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Countries/Popular_pages"
+        request_1 = requests.get(popular_pages_url)
+        popularity_data = StringIO(request_1.text)
+        popularity_df = pd.read_html(popularity_data)[0]
+        national_capitals_url = "https://en.wikipedia.org/wiki/List_of_national_capitals"
+        request_2 = requests.get(national_capitals_url)
+        capitals_data = StringIO(request_2.text)
+        capitals_df = pd.read_html(capitals_data)[1]
+        capitals_quiz_data = pd.merge(popularity_df, capitals_df, how='inner', left_on='Page title', right_on='Country/Territory')
+        capitals_quiz_data_relevant_only = capitals_quiz_data[['Country/Territory', 'City/Town', "Continent", "Views"]].copy()
+        capitals_quiz_data_relevant_only["Last Updated"] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        with get_connection() as conn:
+            capitals_quiz_data_relevant_only.to_sql(Config.CAPITALS_TABLE_NAME, conn, if_exists='replace', index=False)
+        return render_template('refresh-data.html', data=capitals_quiz_data_relevant_only.to_html())
+    else:
+        return render_template('refresh-data.html', data="This feature is not available for PostgreSQL databases. Please use SQLite for this feature.")
 
 @app.route('/capitals-quiz', methods=['GET', 'POST'])
 def capitals_quiz():
